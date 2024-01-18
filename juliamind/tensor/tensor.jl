2f23ef3f3c889f3953d21a64565847c3c40602cc
varuni <varunihk7@gmail.com>
2024-01-18 22:24:28 +0530

    created base class Tensor. Added shape , add and multiply functions

--- juliamind/tensor/tensor.jl
+++ juliamind/tensor/tensor.jl
@@ -4,7 +4,8 @@ struct Tensor{T, N}
     data::Array{T, N}
 end
 
-function Tensor(shape::Vararg{Int, N}; dtype=Float64) where N
+function Tensor(dtype=Float64,shape::Vararg{Int, N=missing};) where N
+    
     data = rand(dtype, shape)
     return Tensor(data)
 end
@@ -21,5 +22,25 @@ end
 function multiply(t1::Tensor, t2::Tensor)::Tensor
     return Tensor(t1.data .* t2.data)
 end
+# Create tensors
+tensor1 = Tensor(3, 3)
+tensor2 = Tensor(3, 3)
 
-end #module 
+tensor3 = Tensor(1)
+tensor4 = tensor()
+
+# Display the shape of tensors
+# println("Shape of tensor1: ", shape(tensor1))
+# println("Shape of tensor2: ", shape(tensor2))
+# println(shape(tensor3))
+# println(tensor3)
+# # Perform operations on tensors
+# result_add = add(tensor1, tensor2)
+# result_multiply = multiply(tensor1, tensor2)
+
+# # Display the results
+# println("\nElement-wise addition result:")
+# println(result_add.data)
+
+# println("\nElement-wise multiplication result:")
+# println(result_multiply.data)
